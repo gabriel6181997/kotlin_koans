c556a3f5dde9381c8f284609f6d1d66741b6c2ac
gabriel6181997 <gabriel6181997@gmail.com>
2022-08-04 16:40:39 +0900

    Initial Commit

---
+++ Introduction/q1.kt
@@ -0,0 +1,9 @@
+// Q1 Simple Functions
+
+// Check out the function syntax and change the code to make the function start return the string "OK".
+// In the Kotlin Koans tasks, the function TODO() will throw an exception. To complete Kotlin Koans, you need to replace this function invocation with meaningful code according to the problem. (edited) 
+
+
+fun start():String {
+   return "OK"
+}
