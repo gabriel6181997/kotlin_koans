522ab1c428768a528924e14d58dc854ffd8519f8
gabriel6181997 <gabriel6181997@gmail.com>
2022-08-04 17:44:30 +0900

    Finish Classes Q5

---
+++ Classes/q5.kt
@@ -0,0 +1,14 @@
+// Q5:Extension Function
+// Learn about extension functions. Then implement the extension functions Int.r() and Pair.r() and make them convert Int and Pair to a RationalNumber.
+// Pair is a class defined in the standard library:
+data class Pair<out A, out B>(
+    val first: A,
+    val second: B
+)
+
+fun Int.r(): RationalNumber = RationalNumber(this,1)
+
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(first,second)
+
+data class RationalNumber(val numerator: Int, val denominator: Int)
+
