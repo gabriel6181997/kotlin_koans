f6ac69f318067812716fda488486f033b5fea9e0
gabriel6181997 <gabriel6181997@gmail.com>
2022-08-04 17:14:27 +0900

    Change title of intro q2

--- Introduction/q2.kt
+++ Introduction/q2.kt
@@ -1,4 +1,4 @@
-// Introduction Q2 Named arguments
+// Q2 Named arguments
 // Make the function joinOptions() return the list in a JSON format (for example, [a, b, c]) by specifying only two arguments.
 // Default and named arguments help to minimize the number of overloads and improve the readability of the function invocation. The library function joinToString is declared with default values for parameters: (edited)
 
