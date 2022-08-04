2ac2f1518c89acf0bd030640781fdcc1e5e1333f
gabriel6181997 <gabriel6181997@gmail.com>
2022-08-04 17:25:44 +0900

    FInish Intro Q8

---
+++ Introduction/q8.kt
@@ -0,0 +1,6 @@
+// Q8: Lambdas
+// Kotlin supports functional programming. Learn about lambdas in Kotlin.
+// Pass a lambda to the any function to check if the collection contains an even number. The any function gets a predicate as an argument and returns true if at least one element satisfies the predicate.
+
+fun containsEven(collection: Collection<Int>): Boolean =
+        collection.any { it%2 == 0 }
