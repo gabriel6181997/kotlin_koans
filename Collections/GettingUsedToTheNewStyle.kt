2ae74e3a869d2c2c1ed8be51951c68a000dc7c5d
gabriel6181997 <gabriel6181997@gmail.com>
2022-08-10 13:14:49 +0900

    Finish Collections Getting used to the new style

---
+++ Collections/GettingUsedToTheNewStyle.kt
@@ -0,0 +1,40 @@
+// Getting used to the new style
+// We can rewrite and simplify the following code using lambdas and operations on collections.
+// Fill in the gaps in doSomethingWithCollection, the simplified version of the doSomethingWithCollectionOldStyle function, so that its behavior stays the same and isn't modified in any way.
+
+fun doSomethingWithCollectionOldStyle(
+    collection: Collection<String>
+): Collection<String>? {
+    val groupsByLength = mutableMapOf<Int, MutableList<String>>()
+    for (s in collection) {
+        var strings: MutableList<String>? = groupsByLength[s.length]
+        if (strings == null) {
+            strings = mutableListOf()
+            groupsByLength[s.length] = strings
+        }
+        strings.add(s)
+    }
+
+    var maximumSizeOfGroup = 0
+    for (group in groupsByLength.values) {
+        if (group.size > maximumSizeOfGroup) {
+            maximumSizeOfGroup = group.size
+        }
+    }
+
+    for (group in groupsByLength.values) {
+        if (group.size == maximumSizeOfGroup) {
+            return group
+        }
+    }
+    return null
+}
+
+fun doSomethingWithCollection(collection: Collection<String>): Collection<String>? {
+
+  val groupsByLength = collection.groupBy { s -> s.length }
+
+  val maximumSizeOfGroup = groupsByLength.values.map { group -> group.size }.maxOrNull()
+
+  return groupsByLength.values.firstOrNull { group -> group.size == maximumSizeOfGroup }
+}
